04d0c5d09f31d1e113fb7455bebfa0fc76f36c39
codinister <codinister@gmail.com>
2026-03-23 13:50:28 +0100

    Updated link tag in Ad component

--- components/post/Ads.tsx
+++ components/post/Ads.tsx
@@ -2,7 +2,6 @@
 
 import useGetQuery from '@/state/query/useGetQuery';
 import Image from 'next/image';
-import Link from 'next/link';
 
 const Ads = () => {
   const data = useGetQuery('ads', '/ads') || [];
@@ -12,9 +11,9 @@ const Ads = () => {
       {data.length > 0
         ? data.map(
             (v: { image: string; title: string; link: string }, k: number) => (
-              <Link key={k} href={v.link} className="block mb-4">
+              <a  key={k} href={v.link} target="_blank" className="block mb-4">
                 <Image src={v.image} width={700} height={900} alt="" />
-              </Link>
+              </a>
             ),
           )
         : ''}
