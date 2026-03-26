638a17b0e03eb53cec64c581cd5c9928ffbaeac9
codinister <codinister@gmail.com>
2026-03-26 19:45:38 +0100

    Updated single item page query

--- app/api/general/[id]/route.ts
+++ app/api/general/[id]/route.ts
@@ -1,7 +1,4 @@
-import Links from '@/components/nav/Links';
-import serverConfig from '@/state/sanity/server.config';
-import { ItemTypes } from '@/types/types';
-import { groq } from 'next-sanity';
+import getProductById from '@/lib/getProductById';
 import { NextRequest, NextResponse } from 'next/server';
 export const revalidate = 0;
 export const dynamic = 'force-dynamic';
@@ -11,48 +8,9 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> },
 ) {
   const id = (await params).id;
-
-  const date = new Date().toISOString();
-
   try {
-    const data = await serverConfig.fetch(
-      groq`
-    *[_id == $id]{
-    'type': _type, 
-    'createdAt' : _createdAt, 
-    'updatedAt' : _updatedAt,
-    'id': _id,
-    title,
-    "cat" : category->title, 
-    "colour": colour[]->{
-    title,
-    hex
-    },
-    "size": sizes[]->title,
-    "description": items.description,
-    "excerpt": array::join(string::split((pt::text(items.description)), "")[0..200], "")+"...",
-    "features": items.features[]{
-    title, 
-    sub_title, 
-    body
-    },
-    "gallery": items.gallery.images[].img.asset->url,
-    "price": items.price,
-    "product": items.product,
-    "stock": items.stock,
-    "sub_title": items.sub_title ,
-    "thumbnail": items.thumbnail.img.asset->url
-    
-    }
-    `,
-      { id },
-    );
-    return NextResponse.json(
-      data.map((v: ItemTypes) => ({
-        ...v,
-        date,
-      })),
-    );
+    const res = await getProductById(id);
+    return NextResponse.json(res);
   } catch (err) {
     console.log(err);
   }
---
+++ lib/getProductById.ts
@@ -0,0 +1,55 @@
+
+import serverConfig from '@/state/sanity/server.config';
+import { ItemTypes } from '@/types/types';
+import { groq } from 'next-sanity';
+import getDollarRate from './getDollarRate';
+
+const getProductById = async (id: string) => {
+  const date = new Date().toISOString();
+  const rate = await getDollarRate();
+
+  const sett = await serverConfig.fetch(groq`*[_type == 'settings'].currency`);
+
+  const data = await serverConfig.fetch(
+    groq`
+        *[_id == $id]{
+          'type': _type, 
+          'createdAt' : _create5dAt, 
+          'updatedAt' : _updatedAt,
+          'id': _id,
+          title,
+          "cat" : category->title, 
+          "colour": colour[]->{
+          title,
+          hex
+          },
+          "size": sizes[]->title,
+          "description": items.description,
+          "excerpt": array::join(string::split((pt::text(items.description)), "")[0..200], "")+"...",
+          "features": items.features[].title,
+          "gallery": items.gallery.images[].img.asset->url,
+          "price": items.price,
+          "product": items.product,
+          "stock": items.stock,
+          "sub_title": items.sub_title ,
+          "thumbnail": items.thumbnail.img.asset->url
+        }
+    `,
+    { id },
+  );
+
+  return data.map((v: ItemTypes) => {
+    const cedi = Number(v.price);
+    const usd = Number(rate * v.price);
+
+    return {
+      ...v,
+      date,
+      cediPrice: cedi,
+      dollarPrice: Math.floor(usd),
+      price: sett[0] === '$' ? Math.floor(usd) : cedi,
+    };
+  });
+};
+
+export default getProductById
