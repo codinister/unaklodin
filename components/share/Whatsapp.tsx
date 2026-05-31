e0f8d6e53faf240cbf8a8288dd9f9be3151fc2b0
codinister <codinister@gmail.com>
2026-05-31 17:40:43 +0100

    product share bbtns

--- components/share/Whatsapp.tsx
+++ components/share/Whatsapp.tsx
@@ -5,12 +5,16 @@ import { WhatsappIcon } from 'react-share';
 const Whatsapp = ({ phone, url }: { phone: number; url: string }) => {
   const encodeUrl = encodeURIComponent(url);
 
-
-  return (
-    <a href={`https://wa.me/${phone}?text=${encodeUrl}`}>
-      <WhatsappIcon size={40} round />
-    </a>
-  );
+  if (phone && encodeUrl) {
+    return (
+      <a
+        href={`https://wa.me/${phone}?text=${encodeUrl + ' ' + 'Hi%20there%21%20I%27m%20interested%20in%20this%20product.%20Is%20it%20still%20available%3F'}`}
+      >
+        <WhatsappIcon size={40} round />
+      </a>
+    );
+  }
+  return ''
 };
 
 export default Whatsapp;
