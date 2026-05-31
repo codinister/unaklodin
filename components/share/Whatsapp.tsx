666542fbd613814bc88cb8ae3559e17769db5579
codinister <codinister@gmail.com>
2026-05-31 17:52:33 +0100

    added whatsapp btn

--- components/share/Whatsapp.tsx
+++ components/share/Whatsapp.tsx
@@ -1,20 +1,24 @@
 'use client';
 
 import { WhatsappIcon } from 'react-share';
+import { useState, useEffect } from 'react';
 
 const Whatsapp = ({ phone, url }: { phone: number; url: string }) => {
+  const [getPhone, setPhone] = useState(0);
+  const [getUrl, setUrl] = useState('');
   const encodeUrl = encodeURIComponent(url);
+  useEffect(() => {
+    setPhone(phone);
+    setUrl(encodeUrl);
+  }, [phone, encodeUrl]);
 
-  if (phone && encodeUrl) {
-    return (
-      <a
-        href={`https://wa.me/${phone}?text=${encodeUrl + ' ' + 'Hi%20there%21%20I%27m%20interested%20in%20this%20product.%20Is%20it%20still%20available%3F'}`}
-      >
-        <WhatsappIcon size={40} round />
-      </a>
-    );
-  }
-  return ''
+
+
+  return (
+    <a href={`https://wa.me/${getPhone}?text=${getUrl}`}>
+      <WhatsappIcon size={40} round />
+    </a>
+  );
 };
 
 export default Whatsapp;
