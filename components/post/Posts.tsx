3bea50ae9b4faa8dac2cf9a95b44eeb449d3e21f
codinister <codinister@gmail.com>
2026-09-14 17:51:27 +0100

    post sort

--- components/post/Posts.tsx
+++ components/post/Posts.tsx
@@ -8,11 +8,19 @@ const Posts = () => {
 
   return (
     <>
-      {data.map(
-        (v: { title: string; thumb: string; excerpt: string; id: string }, k: number) => {
-          return <PostCard key={k} v={v} />;
-        },
-      )}
+      {data
+        .sort(
+          (a: { createdAt: string }, b: { createdAt: string }) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        )
+        .map(
+          (
+            v: { title: string; thumb: string; excerpt: string; id: string },
+            k: number,
+          ) => {
+            return <PostCard key={k} v={v} />;
+          },
+        )}
     </>
   );
 };
